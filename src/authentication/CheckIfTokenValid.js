8171bff9fe7e2bd11c467104b97c02d2694920d2
Justyna <justyna.lisok@gmail.com>
2020-11-04 21:14:41 +0100

    [RTR] hotfix to checking if token valid

--- src/authentication/CheckIfTokenValid.js
+++ src/authentication/CheckIfTokenValid.js
@@ -4,7 +4,7 @@ import {setCredentialsForUnauthenticatedUser} from "./setCredentialsForUnauthent
 
 export function checkIfTokenValid(props) {
 
-    if (localStorage.getItem(LocalStorageItemNames.TOKEN_EXPIRATION_TIME).length > 0) {
+    if (localStorage.getItem(LocalStorageItemNames.TOKEN_EXPIRATION_TIME) !== null) {
         const jwtExpiration = moment(localStorage.getItem(LocalStorageItemNames.TOKEN_EXPIRATION_TIME));
         if (jwtExpiration.diff(moment.now()) < 0) {
             setCredentialsForUnauthenticatedUser();
